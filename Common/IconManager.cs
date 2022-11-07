417b09cade1337e85618db4a82c1c48c98133a31
Vitalii Shuliak <kl1mes@ex.ua>
2022-11-07 22:09:42 +0200

    Add project files.

---
+++ Common/IconManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows.Media.Imaging;
+using System.Reflection;
+
+namespace InstallConduitsStrap.Common
+{
+    public class IconManager
+    {
+        /// <summary>
+        /// Gets the icon from reource assembly.
+        /// </summary>
+        /// <param iconName="iconName">The iconName.</param>
+        /// <returns></returns>
+        public static BitmapImage GetIcon(string iconName) 
+        {
+
+            // Create the resource reader stream.
+            var stream = Assembly.GetExecutingAssembly()
+                .GetManifestResourceStream(typeof(App).Namespace+".Icons."
+                                                                + iconName);
+
+            //Construct and return icon.
+            var icon = new BitmapImage();
+            icon.BeginInit();
+            icon.StreamSource = stream;
+            icon.EndInit();
+                   
+
+            //Return constructed BitmapImage.
+            return icon;
+        }
+    }
+}
---
+++ Common/RevitMessage.cs
@@ -0,0 +1,48 @@
+﻿using Autodesk.Revit.UI;
+
+namespace InstallConduitsStrap.Common
+{
+    public class RevitMessage
+    {
+        /// <summary>
+        /// Display the specify message.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="type"></param>
+        #region public methods
+        public static void Display(string message, WindowType type)
+        {
+            string title = "";
+            var icon = TaskDialogIcon.TaskDialogIconNone;
+
+            //Customize window based on type of message.
+            switch (type)
+            {
+                case WindowType.Information:
+                    title = "~ INFORMATION ~";
+                    icon = TaskDialogIcon.TaskDialogIconInformation;
+                    break;
+                case WindowType.Warning:
+                    title = "~ WARNING ~";
+                    icon = TaskDialogIcon.TaskDialogIconWarning;
+                    break;
+                case WindowType.Error:
+                    title = "~ ERROR ~";
+                    icon = TaskDialogIcon.TaskDialogIconError;
+                    break;
+                default:
+                    break;
+            }
+            // Construction window to display specified message.
+            var window = new TaskDialog(title)
+            {
+                MainContent = message,
+                MainIcon = icon,
+                CommonButtons = TaskDialogCommonButtons.Ok
+            };            
+            window.Show();
+        }
+
+        #endregion
+    }
+}
